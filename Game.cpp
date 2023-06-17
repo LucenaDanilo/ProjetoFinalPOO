711500e75661c70a0c59a13c5c245538e92761cf
Lucena.danilo <danilo.lucena.ext@synchro.com.br>
2023-06-16 21:51:11 -0300

    Adicionando sprites e a classe helicoptero

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.hpp"
 #include "SpriteBase.hpp"
 #include "Fase.hpp"
+#include "ObjetoDeJogo.hpp"
 
 Sprite Game::screen("./Sprites/screen");
 Sprite Game::screenLimpa("./Sprites/screen");
@@ -9,13 +10,9 @@ void Game::run() {
 
     Fase fase1;
     fase1.init();
+    fase1.draw();
     
     // std::string s;
     // getline(std::cin, s);
     // system("clear");
 }
-
-// void Game::reapontar() {
-//     delete Game::pScreen;
-//     Game::pScreen = new Sprite("./Sprites/screen");
-// }
